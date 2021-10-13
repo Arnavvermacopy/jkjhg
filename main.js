867b4edc6be377a4509a88ac1a2301e771843503
Arnavvermacopy <91061021+Arnavvermacopy@users.noreply.github.com>
2021-10-13 17:44:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name="Arnav";
+var last_name="Verma";
+var a =5;
+var b=9;
+console.log("My first console project");
+
+console.log(first_name.concat(last_name));
+
+var add =a+b;
+console.log(add);
